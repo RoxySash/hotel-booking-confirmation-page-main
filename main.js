4071c437ee1f075631b9bec8410a10ca328a858c
Roxane Hedge <roxanehedge@gmail.com>
2026-06-29 16:03:06 -0500

    added copy password function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const openBtn = document.getElementById("openBtn");
 const closeBtn = document.getElementById("closeBtn");
-
-
+const wifiPassword = document.getElementById("wifiPassword");
+const copyBtn = document.getElementById("copyBtn");
 
 function openNav() {
 
@@ -14,3 +14,19 @@ function closeNav() {
 
 closeBtn.addEventListener("click", closeNav);
 openBtn.addEventListener("click", openNav);
+
+const wifiText = wifiPassword.innerHTML;
+
+const copyContent = async () => {
+    try {
+      await navigator.clipboard.writeText(wifiText);
+      alert("copied")
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+  }
+
+copyBtn.addEventListener("click", copyContent);
+
+
+
